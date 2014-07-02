34ceea93a05acbb05f4b54d957c1bb68296cc3c0
xy19xiaoyu <0714010877@163.com>
2014-07-02 22:28:50 +0800

    xy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,48 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var where='';
+function regeditcheck()
+{
+    $(".coolfieldset :checkbox").each(function (){
+        $(this).click(function(){
+            debugger;
+            var id =$(this).attr('id').replace(/\s/g,'');
+            var tmpwhere = id  +"=1";
+            if($(this).prop("checked")==true)
+            {
+                if(where.indexOf(tmpwhere)>0)
+                {
+                    //todo:nothing;
+                }
+                else
+                {
+                    if(where =="")
+                    {
+                        where +=  tmpwhere;
+                    }
+                    else
+                    {
+                        where += "&" + tmpwhere;
+                    }
+
+                }
+            }
+            else
+            {
+                where = where.replace("&" + tmpwhere,"");
+            }
+            alert(where);
+            var href= window.location.href;
+            if(href.indexOf("?")>0)
+            {
+                href = href.substr(0,href.indexOf("?"));
+            }
+           // window.location.href = href +"?" + where
+            
+
+        });
+
+    });
+}
